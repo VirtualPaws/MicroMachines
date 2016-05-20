3375b57fec374187cbeb45370089d88241977f85
s65249 <yell.king@gmx.de>
2016-05-20 10:56:38 +0200

    More Networking work

---
+++ Assets/Scripts/Networking/PlayerRep.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class PlayerRep : NetworkBehaviour {
+
+    public int id = -1;
+
+    [SyncVar]
+    public bool isReady = false;
+    public GameObject carPrefab;
+
+	// Use this for initialization
+	void Start () {
+        GameObject go = GameObject.Find("NetworkManager");
+        PlayerManagement pm = (PlayerManagement)go.GetComponent(typeof(PlayerManagement));
+        id = pm.playerRepresentations.Count;
+        pm.playerRepresentations.Add(gameObject);
+
+        NetworkManager nm = (NetworkManager)gameObject.GetComponent(typeof(NetworkManager));
+        if (SceneManager.GetActiveScene().name == "testTrack_2")
+        {
+            GameObject car = (GameObject)Instantiate(carPrefab, transform.position, transform.rotation);
+            NetworkServer.Spawn(car);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
