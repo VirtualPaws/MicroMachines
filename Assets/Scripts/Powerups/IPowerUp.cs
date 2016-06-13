abb68f1384ae53b6f9e62e332b3d4d3d4c1daa7a
s65249 <yell.king@gmx.de>
2016-06-13 18:21:35 +0200

    Rocketry

--- Assets/Scripts/Powerups/IPowerUp.cs
+++ Assets/Scripts/Powerups/IPowerUp.cs
@@ -20,3 +20,28 @@ public class SpeedBoostPowerUp : IPowerUp
         return "Speed Boost";
     }
 }
+
+public class RocketPowerUp : IPowerUp
+{
+    private GameObject rocketPrefab = null;
+    private Vector3 offset = new Vector3(0,5,0);
+
+    public void fire(GameObject firingFrom)
+    {
+        Vector3 origin = firingFrom.transform.position;
+        Vector3 originForce = firingFrom.GetComponent<Rigidbody>().velocity;
+        if (rocketPrefab == null)
+        {
+            rocketPrefab = Resources.Load("attacks/Rocket", typeof(GameObject)) as GameObject;
+        }
+        GameObject rocket = GameObject.Instantiate(rocketPrefab);
+        rocket.transform.position = origin + offset;
+        rocket.GetComponent<Rigidbody>().velocity = originForce;
+        rocket.GetComponent<AttackRocket>().toIgnore.Add(firingFrom);
+    }
+
+    public string getName()
+    {
+        return "Rocket";
+    }
+}
