6d0014cefed966569ead7a17b9b6fa469f3f27d9
kkonkol <s65199@beuth-hochschule.de>
2016-06-16 19:23:25 +0200

    close camera

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+	public GameObject car1;
+	public GameObject car2;
+	public GameObject car3;
+
+	// Use this for initialization
+	void Start () {
+		car1.SetActive (true);
+		car2.SetActive (true);
+		car3.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey) {
+			SceneManager.LoadScene ("ChoiceScene");
+		}
+	}
+}
