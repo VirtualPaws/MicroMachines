83d8ba055115b36492fe9a83ab366d644b581553
kkonkol <s65199@beuth-hochschule.de>
2016-07-14 21:51:42 +0200

    audiosources, klo ist jetzt fort nox

---
+++ Assets/MusicHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicHandler : MonoBehaviour {
+
+	//credits to jashan, unityforum
+	private static MusicHandler instance = null;
+	public static MusicHandler Instance{
+		get { return instance; }
+	}
+
+	void Awake(){
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+	public void playNextSound(){
+		
+	}
+}
