e12a4418c1e02c4dd5c8403aecfa2f6cf9f5a33a
s65249 <yell.king@gmx.de>
2016-07-14 19:47:08 +0200

    Added Player Labels

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+    public GameObject toFace;
+	
+	// Update is called once per frame
+	void Update () {
+        if (toFace == null)
+        {
+            return;
+        }
+        this.transform.LookAt(toFace.transform.position);
+        this.transform.Rotate(new Vector3(0, 180, 0));
+	}
+}
